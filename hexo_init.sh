7a54bb8b5e44095e28729b1b04bdcfc688dff3bc
chengyi818 <chengyi818@foxmail.com>
2016-04-08 10:44:38 +0800

    update

--- hexo_init.sh
+++ hexo_init.sh
@@ -5,14 +5,10 @@ sudo apt-get install git npm nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 #安装hexo
-sudo npm install -g hexo-cli
+npm install -g hexo-cli
 
 #安装插件
-sudo npm install hexo-deployer-git --save
-sudo npm install hexo-generator-feed --save
-sudo npm install hexo-generator-sitemap --save
-sudo npm install markdown-it-emoji --save
-
-sudo npm un hexo-renderer-marked --save
-sudo npm i hexo-renderer-markdown-it --save
+npm install hexo-deployer-git --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-sitemap --save
 
