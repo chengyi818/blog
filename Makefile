13ce773d744811113d1e834fdd7b22087d838959
chengyi818 <chengyi818@foxmail.com>
2017-04-02 07:14:44 +0800

    omment

---
+++ Makefile
@@ -0,0 +1,7 @@
+update:
+	read -p "Please input your comment:" comment; \
+	hexo clean; \
+	hexo d -g; \
+	git add -A; \
+	git commit -m "$comment"; \
+	git push origin master
--- my_shell_scripts/hexo_init.sh
+++ my_shell_scripts/hexo_init.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 #安装必备环境
 sudo apt-get update
-sudo apt-get install git  nodejs npm
-#sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install git nodejs npm
 
 #安装hexo
 sudo npm install -g hexo-cli
-
-#安装插件
-sudo npm install hexo-deployer-git --save
-
-sudo npm install hexo --save
+sudo npm install
